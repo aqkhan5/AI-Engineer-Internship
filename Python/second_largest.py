7f649582505dc971c54265f8fb6bdecedecb274a
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-08-07 09:55:46 +0500

    Update second_largest.py

---
+++ Python/second_largest.py
@@ -0,0 +1,13 @@
+# Finding the second largest in the list
+numbers = [12, 45, 7, 89, 34, 89, 56]
+largest = numbers[0]
+second_largest = 0
+
+for num in numbers:
+    if num > largest:
+        second_largest = largest
+        largest = num
+    elif num > second_largest & num != largest:
+        second_largest = num
+
+print(f"The Second largest number is {second_largest}")
