03d6a9f6d3f3c321065df2799bdeb6af82a6e7c4
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-08-07 12:09:50 +0500

    Update common_number.py

--- Python/common_number.py
+++ Python/common_number.py
@@ -1,4 +1,9 @@
 # Finding the common numbers among the lists
 list1 = [1, 2, 3, 4, 5]
 list2 = [3, 5, 7, 8]
-common_numbers = []
+
+print("printing the common numbers")
+for num1 in list1:
+    for num2 in list2:
+        if num1 == num2:
+            print(num1)
