f64362dd858e66b41396ab7795e7bfafae4e4b78
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-08-12 17:25:29 +0500

    advance exception handling

--- FastAPI practice/fast_basics/advance_Exception_handling.py
+++ FastAPI practice/fast_basics/advance_Exception_handling.py
@@ -1,8 +1,10 @@
 # Advance Exception handling
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+# HTTP Exception    built-in
 @app.get("/users/{user_id}")
 def get_user(user_id : int):
     if user_id != 1:
@@ -13,4 +15,40 @@ def get_user(user_id : int):
     return{
         "id" : 1,
         "name" : "Mohsin"
+    }
+
+# Custom exceptions handling   built-in
+class UserNotFoundException(Exception):
+    def __init__(self, name):
+        self.name = name
+
+@app.get("/user{name}")
+def get_users(name: str):
+    if name != "mohsin":
+        raise UserNotFoundException(name)
+    return {
+        "name" : name
+    }
+
+# Global Errors   Customized
+class UserNotFoundException(Exception):
+    def __init__(self, name):
+        self.name = name
+
+@app.exception_handler(UserNotFoundException)
+def user_not_found_handler(request : Request, exc: UserNotFoundException):
+    return JSONResponse(
+        status_code= 404,
+        content={
+            "status": "Error",
+            "message" : f" User {exc.name} not found"
+        }
+    )
+
+@app.get("/user{name}")
+def get_users(name: str):
+    if name != "mohsin":
+        raise UserNotFoundException(name)
+    return {
+        "name" : name
     }
