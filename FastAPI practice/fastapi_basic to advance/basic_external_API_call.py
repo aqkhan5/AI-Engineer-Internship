db64bcaced21fa5548a4a8de6c9fa9904f0e777a
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-08-19 12:57:59 +0500

    .

--- FastAPI practice/fastapi_basic to advance/basic_external_API_call.py
+++ FastAPI practice/fastapi_basic to advance/basic_external_API_call.py
@@ -7,20 +7,26 @@
 
 # print(data[:2])
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
 app = FastAPI()
 
 @app.get("/posts")
 def get_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(url)
 
+    response = requests.get(url)
     return response.json()
 
 @app.get("/posts/{post_id}")
 def get_a_post(post_id: int):
-    url = "https://jsonplaceholder.typicode.com/posts"
+    url = f"https://jsonplaceholder.typicode.com/posts/{post_id}"
+
     response = requests.get(url)
 
-    return response.json()
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code= 404,
+            detail= "Data Not Found!"
+        )
+    return response.json() 
