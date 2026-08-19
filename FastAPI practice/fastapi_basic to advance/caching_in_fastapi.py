a002a32bff0812d07530aa8b9d71f836e2368d74
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-08-19 18:23:37 +0500

    practicing fastapi

---
+++ FastAPI practice/fastapi_basic to advance/caching_in_fastapi.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException
+import requests
+from bs4 import BeautifulSoup
+import time
+
+app = FastAPI()
+
+cache_data = []
+last_fetch = 0
+
+
+@app.get("/news")
+def get_news():
+    global cache_data, last_fetch
+    start = time.time()
+
+    if time.time() - last_fetch > 60:
+        print("Fetching fresh data!")
+
+        url = "https://news.ycombinator.com/"
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        cache_data = [
+            item.text for item in soup.find_all("span", class_="titleline")
+        ]
+
+        last_fetch = time.time()
+
+    else:
+        print("Using Cache Data!")
+
+    end = time.time()
+    time_taken = round(end-start, 4)
+    print("Time taken " , time_taken)
+
+    return{
+        "time_taken" : time_taken,
+        "data" : cache_data
+    }
