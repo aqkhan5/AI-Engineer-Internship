f076387f6563b8c310be77b98efbc6c18d423acc
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-08-25 14:54:14 +0500

    practicing alembic

--- FastAPI practice/alembic_demo/database.py
+++ FastAPI practice/alembic_demo/database.py
@@ -8,10 +8,12 @@ sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = "users"
-
+    
     id = Column(Integer, primary_key = True, index = True)
     email = Column(String, unique = True, index = True, nullable = False)
     hashed_password = Column(String, nullable = False)
     is_active = Column(Boolean, default=True)
+
