352e9318959d27fdc3a26efa9214c0e23bb5fcb8
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-08-31 18:54:52 +0500

    advance BlogAPI

--- FastAPI practice/advance_blog_api/routers/posts.py
+++ FastAPI practice/advance_blog_api/routers/posts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 import models, schemas, utils
@@ -41,3 +41,59 @@ def get_posts(db: Session = Depends(get_db)):
         )
         for post in posts
     ]
+
+@router.put("/post_id",response_model= List[schemas.PostResponse])
+def update_post(
+    post_id: int,
+    updated_post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(utils.verify_current_token)
+    ):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if not post:
+        raise HTTPException(
+            status_code= 404,
+            detail= " Post Not Found!"
+        )
+    if post.user_id != current_user.id:
+        raise HTTPException(
+            status_code= 403,
+            detail= " Not Authorized!"
+        )
+    post.title = updated_post.title
+    post.content= updated_post.content
+
+    db.commit()
+    db.refresh(post)
+
+    return schemas.PostResponse(
+            id = post.id,
+            title = post.title,
+            content = post.content,
+            author = current_user.username
+
+    )
+
+# Delete post
+@router.delete("/post_id")
+def update_post(
+    post_id: int,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(utils.verify_current_token)
+    ):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if not post:
+        raise HTTPException(
+            status_code= 404,
+            detail= " Post Not Found!"
+        )
+    if post.user_id != current_user.id:
+        raise HTTPException(
+            status_code= 403,
+            detail= " Not Authorized!"
+        )
+  
+    db.delete(post)
+    db.commit()
+
+    return {"Details" : "Post Deleted Successfully"}
