9307ce32b8f9fb62eb9b2cd33e5e18aabe4bd5e4
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-09-01 15:50:15 +0500

    QR Code

---
+++ FastAPI practice/QR_Code_Scanner/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import qrcode
+import io, base64
+
+app = FastAPI()
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins= ["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
+
+@app.post("/generate_QR")
+async def generate_qr(request: Request):
+    data = await request.json()
+    text = data.get("text", "")
+
+    if not text.strip():
+        return JSONResponse({"error": "The text cannot be empty"}, status_code= 400 )
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_collor = "black", back_color = "white")
+
+    buffer = io.BytesIO()
+    img.save(buffer, format="PNG")
+    qr_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    return {"qr_code": qr_base64}
